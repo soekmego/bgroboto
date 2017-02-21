53b4731b0d4375a386c2cae8a914616035d15222
soekmego <soekmen.goekhan@gmail.com>
2017-02-21 01:21:41 +0100

    added hotfix to testscript

--- mousemovement.py
+++ mousemovement.py
@@ -1,5 +1,10 @@
 import pyautogui
 
+#to escape a mouse out of control, move the mouse quickly to the upper-left
+#corner of the screen
+pyautogui.FAILSAFE = True
+
+
 for i in range(10):
     pyautogui.moveTo(100, 100, duration=0.25)
     pyautogui.moveTo(200, 100, duration=0.25)
