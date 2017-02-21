5052f9c112276b2cedd62a8e32f99cb8d06244ea
soekmego <soekmen.goekhan@gmail.com>
2017-02-21 22:34:50 +0100

    console script

---
+++ mouseNow.py
@@ -0,0 +1,16 @@
+#! python3
+# mouseNow.py - Displays the mouse cursor's current position.
+#
+
+import pyautogui
+
+print("Press Ctrl-C to quit.")
+try:
+    while True:
+        # Get and print the mouse coordinates.
+        x, y = pyautogui.position()
+        positionStr = "X: " + str(x).rjust(4) + " Y: " + str(y).rjust(4)
+except KeyboardInterrupt:
+    print("/nDone.")
+    print(positionStr, end="")
+    print("/b" * len(positionStr), end = "", flush=True)
