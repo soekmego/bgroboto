ba526905a47c13cff6008a2c5b10574c8b9f2985
soekmego <soekmen.goekhan@gmail.com>
2017-02-23 15:24:35 +0100

    hotfix for mouseNow

---
+++ imgrecog.py
@@ -0,0 +1,6 @@
+import os
+from PIL import Image
+
+# if img not in same dir as the working dir, change dir with os.chdir("path")
+solImg = Image.open("solar.png")
+
--- mouseNow.py
+++ mouseNow.py
@@ -10,7 +10,7 @@ try:
         # Get and print the mouse coordinates.
         x, y = pyautogui.position()
         positionStr = "X: " + str(x).rjust(4) + " Y: " + str(y).rjust(4)
+        print(positionStr, end="")
+        print("\b" * len(positionStr), end = "", flush=True)
 except KeyboardInterrupt:
-    print("/nDone.")
-    print(positionStr, end="")
-    print("/b" * len(positionStr), end = "", flush=True)
+    print("\nDone.")
