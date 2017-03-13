c1b1f6fcc806c51f530ffbe4de540757b0c82890
Ghost <soekmen.goekhan@gmail.com>
2017-03-13 22:21:36 +0100

    readability

--- imgrecog.py
+++ imgrecog.py
@@ -2,5 +2,5 @@ import os
 from PIL import Image
 
 # if img not in same dir as the working dir, change dir with os.chdir("path")
-solImg = Image.open("solar.png")
+solarImg = Image.open("solar.png")
 
