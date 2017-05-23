6595964dda241d70c6c088a9236dbbf68f0fdef5
soekmego <soekmen.goekhan@gmail.com>
2017-05-23 19:23:23 +0200

    cleanup for readability

--- mouseNow.py
+++ mouseNow.py
@@ -7,10 +7,12 @@ print("Press Ctrl-C to quit.")
 try:
     while True:
         # Get and print the mouse coordinates.
+
         x, y = pyautogui.position()
         positionStr = "X: " + str(x).rjust(4) + " Y: " + str(y).rjust(4)
         print(positionStr, end="")
         print("\b" * len(positionStr), end = "", flush=True)
+
 	#the end = "" argument to print prevents a newline character.
 	#flush=True has to be called when a backspace character is used,
 	#otherwise the screen might not update the text
